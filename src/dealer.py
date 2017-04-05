9d0ff500c3a9d41b779b0038d8bfcebe1d425842
Jenn Le <jenn.thakugan@icloud.com>
2017-04-05 11:24:09 -0500

    Bug fix attempt

--- src/dealer.py
+++ src/dealer.py
@@ -11,38 +11,38 @@ class Dealer(object):
         self.slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
 
     # Admin commands
-    def addplayer(self, command):
-        api_call = slack_client.api_call("users.list")
-        if api_call.get('ok'):
-
-            # retrieve all users so we can add the desired ones to the players list
-            users = api_call.get('members')
-            for user in users:
-                if 'name' in user and user.get('name') in command:
-                    for player in self.players:
-
-                        # Send a message if the player is already in the list
-                        if player.get('name') == user.get('name'):
-                            response = user.get('name') + " has already been added to the player list."
-                            slack_client.api_call("chat.postMessage", text=response, as_user=True)
-                            continue
-
-                    # If the player wasn't in the list, add them
-                    self.players.append({"name": user.get('name'),
-                                         "id": user.get('id'),
-                                         "balance": 500,
-                                         "hand": []
-                                        })
-
-                    response = user.get('name') + " has been added the player list."
-                    slack_client.api_call("chat.postMessage", text=response, as_user=True)
-
     def admin_do(self, command):
+        def addplayer(self, command):
+            api_call = slack_client.api_call("users.list")
+            if api_call.get('ok'):
+
+                # retrieve all users so we can add the desired ones to the players list
+                users = api_call.get('members')
+                for user in users:
+                    if 'name' in user and user.get('name') in command:
+                        for player in self.players:
+
+                            # Send a message if the player is already in the list
+                            if player.get('name') == user.get('name'):
+                                response = user.get('name') + " has already been added to the player list."
+                                slack_client.api_call("chat.postMessage", text=response, as_user=True)
+                                continue
+
+                        # If the player wasn't in the list, add them
+                        self.players.append({"name": user.get('name'),
+                                             "id": user.get('id'),
+                                             "balance": 500,
+                                             "hand": []
+                                            })
+
+                        response = user.get('name') + " has been added the player list."
+                        slack_client.api_call("chat.postMessage", text=response, as_user=True)
+
         actions = {"addplayer": addplayer
                 #    "change_balance": change_balance
                   }
 
-        actions[command.split(' ', 1)[0]](self, command)
+        actions[command.split(' ')[1]](self, command)
 
     # def change_balance(self, command):
     #
